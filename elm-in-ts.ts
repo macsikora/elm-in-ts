b9cf6825d95e5f93ff69b01cca2111fa903171c7
macsikora <62876951+macsikora@users.noreply.github.com>
2021-03-29 11:22:50 +0200

    Create elm-in-ts.ts
    First version of matching, only type level. Sum types based on Lists. Additional Maybe, Result and fromNullable

---
+++ elm-in-ts.ts
@@ -0,0 +1,229 @@
+// helper type
+type UnionToIntersection<U> = 
+  (U extends any ? (k: U)=>void : never) extends ((k: infer I)=>void) ? I : never
+type Sum = [string] | [string, unknown]
+
+// matching single nested type
+type MatchSingle<T extends Sum, R> = 
+// Three levels deep
+UnionToIntersection<T extends [infer A, infer B] ?
+(
+    B extends [infer BA, infer BB] ?
+    (
+        BB extends [infer CA, infer CB] ?
+        (
+            {
+                [K in A & string]:
+                {
+                    [K2 in BA & string]:
+                    {
+                        [K3 in CA & string]: 
+                        {
+                            [K4 in `${K}_${K2}_${K3}`]:
+                                (x: CB) => R
+                        }
+                    }
+                }
+            }[A & string][BA & string][CA & string]
+        ) :
+        BB extends [infer CA] ?
+        (
+            {
+                [K in A & string]:
+                {
+                    [K2 in BA & string]:
+                    {
+                        [K3 in CA & string]: 
+                        {
+                            [K4 in `${K}_${K2}_${K3}`]:
+                                () => R
+                        }
+                    }
+                }
+            }[A & string][BA & string][CA & string]
+        ) : 
+        {
+            [K in A & string]:
+            {
+                [K2 in BA & string]:
+                {
+                    [K3 in `${K}_${K2}`]: 
+                        (x: BB) => R
+                }
+            }
+        }[A & string][BA & string]
+    ) : 
+    B extends [infer BA] ?
+    {
+        [K in A & string]:
+        {
+            [K2 in BA & string]:
+            {
+                [K3 in `${K}_${K2}`]: 
+                    () => R
+            }
+        }
+    }[A & string][BA & string] :
+    {
+        [K in A & string]:
+            (x: B) => R
+    }
+) : 
+T extends [infer A] ?
+    {
+        [K in A & string]:
+            () => R
+    } : 
+    never
+> |
+// Two levels deep
+UnionToIntersection<T extends [infer A, infer B] ?
+(
+    B extends [infer BA, infer BB] ?
+    {
+        [K in A & string]:
+        {
+            [K2 in BA & string]:
+            {
+                [K3 in `${K}_${K2}`]: 
+                    (x: BB) => R
+            }
+        }
+    }[A & string][BA & string] :
+    B extends [infer BA] ?
+    {
+        [K in A & string]:
+        {
+            [K2 in BA & string]:
+            {
+                [K3 in `${K}_${K2}`]: 
+                    () => R
+            }
+        }
+    }[A & string][BA & string] :
+    {
+        [K in A & string]:
+            (x: B) => R
+    } 
+) :
+T extends [infer A] ?
+    {
+        [K in A & string]:
+            () => R
+    } : 
+    never
+> |
+// One level deep
+UnionToIntersection<T extends [infer A, infer B] ?
+{
+    [K in A & string]:
+        (x: B) => R
+} :
+(    
+    T extends [infer A] ?
+    {
+        [K in A & string]:
+            () => R
+    } : 
+    never
+)> |
+{
+    _: () => R //wildcard
+}
+
+type Pattern<T extends Sum, R> = {
+    [K in T[0] | '_']: 
+        T extends [K, infer B] ? B:
+        never
+}
+
+// matching tuple
+type SumTuple = [Sum, Sum] | [Sum, Sum, Sum]
+type MatchMulti<
+    T extends SumTuple, 
+    R, 
+    Tmp extends Record<0 | 1 | 2, Pattern<Sum, R>>= 
+    {
+    [K in keyof T & 0 | 1 | 2]: 
+        Pattern<T[K], R>
+    }
+> = 
+    T extends [Sum, Sum, Sum] ?
+    {
+        [K in keyof Tmp[0] & string]: {
+            [K1 in keyof Tmp[1] & string]: {
+                [K2 in keyof Tmp[2] & string]: {
+                    [K3 in `${K}, ${K1}, ${K2}`]:
+                        Tmp[0][K] extends never ? (
+                            Tmp[1][K1] extends never ? (
+                                Tmp[2][K2] extends never ?
+                                () => R :
+                                (x:Tmp[2][K2]) => R 
+                            ): (
+                                Tmp[2][K2] extends never ?
+                                (x: Tmp[1][K1]) => R :
+                                (x: Tmp[1][K1], y: Tmp[2][K2]) => R
+                            )
+                        ):
+                        Tmp[1][K1] extends never ? (
+                            Tmp[2][K2] extends never ?
+                            () => R :
+                            (x: Tmp[2][K2]) => R
+                        ): 
+                        Tmp[2][K2] extends never ? (
+                            (x: Tmp[0][K], y: Tmp[1][K1]) => R
+                        ):
+                        (x: Tmp[0][K], y: Tmp[1][K1], z: Tmp[2][K2]) => R
+                } & {
+                    '_': () => R
+                }
+            }
+        } 
+    }[keyof Tmp[0] & string][keyof Tmp[1] & string][keyof Tmp[2] & string] :
+    {
+        [K in keyof Tmp[0] & string]: {
+            [K1 in keyof Tmp[1] & string]: {
+                [K2 in `${K}, ${K1}`]:
+                    Tmp[0][K] extends never ? (
+                        Tmp[1][K1] extends never ?
+                        () => R :
+                        (x: Tmp[1][K1]) => R ):
+                    Tmp[1][K1] extends never ? (
+                        (x: Tmp[0][K]) => R 
+                    ): (x: Tmp[0][K], y: Tmp[1][K1]) => R
+            } & {
+                '_': () => R
+            }
+        } 
+    }[keyof Tmp[0] & string][keyof Tmp[1] & string]
+
+
+export type Match<T extends [Sum] | SumTuple, R> = 
+    T extends SumTuple ?
+    MatchMulti<T, R> :
+    T extends [Sum] ?
+    MatchSingle<T[0], R> :
+    never
+
+
+export const match = <V extends [Sum] | SumTuple>(...v: V) => {
+    return {
+        with: <R>(pattern: Match<V, R>): R => {
+            // TO DO implement 🧘
+            // yes there is no value level code yet
+            return 1 as any as R;
+        }
+    }
+}
+
+export type Maybe<A> = 
+    | ['Nothing'] 
+    | ['Just', A];
+export type Result<A, B> = 
+    | ['Ok', A] 
+    | ['Error', B];
+
+export const fromNullable = <T>(x: T): Maybe<NonNullable<T>> => 
+    (x === null || x === undefined) ?
+    ["Nothing"] :
+    ["Just", x as NonNullable<T>]
